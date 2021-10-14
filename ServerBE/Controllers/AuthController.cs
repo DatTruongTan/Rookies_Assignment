a360bf89b932fd9f2688013da4a90f6d30c88df8
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-14 20:47:17 +0700

    Initialized controller for authentication

---
+++ ServerBE/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult("This request has been authorized");
+        }
+    }
+}
