a78bb85b37fbfef43fa6eee8ef1037419599f5a6
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-15 15:28:58 +0700

    Initialized Models for sign in, sign up

---
+++ ServerBE/Models/Auth/SignUpModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Models.Auth
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password need to be comfirmed")]
+        public string ConfirmPassword { get; set; }
+    }
+}
