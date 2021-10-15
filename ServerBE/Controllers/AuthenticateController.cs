53e84002733bbf7f7a6878e83812a0063c295414
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-15 15:30:04 +0700

    Initialized controller for sign in, sign up

---
+++ ServerBE/Controllers/AuthenticateController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ServerBE.Models;
+using ServerBE.Models.Auth;
+using ServerBE.Repository;
+using Shared.UserRole;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AuthenticateController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        //[HttpPost("login")]
+        //public async Task<IActionResult> Login([FromBody] SignInModel signInModel)
+        //{
+        //    var result = await _accountRepository.LoginAsync(signInModel);
+
+        //    if (string.IsNullOrEmpty(result))
+        //    {
+        //        return Unauthorized();
+        //    }
+
+        //    return Ok(result);
+        //}
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpModel signUpModel)
+        {
+            var result = await _accountRepository.SignUpAsync(signUpModel);
+
+            if (result.Succeeded)
+            {
+                return Ok(result.Succeeded);
+            }
+
+            //return Unauthorized();
+            return Content("OK");
+        }
+
+        //[HttpPost]
+        //[Route("register-admin")]
+        //public async Task<IActionResult> RegisterAdmin([FromBody] SignUpModel model)
+        //{
+        //    var userExists = await userManager.FindByNameAsync(model.Username);
+        //    if (userExists != null)
+        //        return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+
+        //    User user = new User()
+        //    {
+        //        Email = model.Email,
+        //        SecurityStamp = Guid.NewGuid().ToString(),
+        //        UserName = model.Username
+        //    };
+        //    var result = await userManager.CreateAsync(user, model.Password);
+        //    if (!result.Succeeded)
+        //        return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+        //    if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+        //        await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+        //    if (!await roleManager.RoleExistsAsync(UserRoles.User))
+        //        await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+        //    if (await roleManager.RoleExistsAsync(UserRoles.Admin))
+        //    {
+        //        await userManager.AddToRoleAsync(user, UserRoles.Admin);
+        //    }
+
+        //    return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+        //}
+    }
+}
---
+++ ServerBE/Repository/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ServerBE.Models.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Repository
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> SignUpAsync(SignUpModel registerModel);
+        Task<string> LoginAsync(SignUpModel registerModel);
+    }
+}
