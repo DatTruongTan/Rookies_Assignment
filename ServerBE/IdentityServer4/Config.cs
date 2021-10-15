e82cdedcf1647744a03648b171b0b63b66d435b2
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-15 18:12:26 +0700

    Config for IS4 - Prepare to add IS4 into project

--- ServerBE/IdentityServer4/Config.cs
+++ ServerBE/IdentityServer4/Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,23 +9,45 @@ namespace ServerBE.IdentityServer4
 {
     public class Config
     {
-        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
-        {
-            new ApiScope("testapis","Test API Scope")
-        };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+            new ApiResource("demoAPI","Test API Scope")
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes => 
+            new List<ApiScope>
+            {
+            new ApiScope("demoAPI","Test API Scope")
+            };
+
         public static IEnumerable<Client> Clients => new List<Client>
         {
             new Client
             {
-                ClientId = "testclient",
+                ClientId = "demoClient",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets =
                 {
-                    new Secret("testsecret".Sha256())
+                    new Secret("demoSecret".Sha256())
                 },
-                AllowedScopes =
+
+                RedirectUris = { "https://localhost:44343/signin-oidc" },
+
+                PostLogoutRedirectUris = { "https://localhost:44343/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
                 {
-                    "testapis"
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "demoAPI"
                 }
             }
         };
