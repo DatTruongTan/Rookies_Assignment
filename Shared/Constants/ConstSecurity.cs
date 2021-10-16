bb6e5aef87b4c1612a839e14565f161992a51e1e
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-16 20:04:43 +0700

    Add constants

---
+++ Shared/Constants/ConstSecurity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Constants
+{
+    public static class ConstSecurity
+    {
+        public const string ADMINSTRATOR_ROLE_CONST = "Administrator";
+        public static string ADMINISTRATION_ROLE = "Administrator";
+        public static string USER_ROLE = "User";
+        public static string BEARER_POLICY = "Bearer";
+        public const string ADMIN_ROLE_POLICY = "ADMIN_ROLE_POLICY";
+    }
+}
