a799b2e408151bd9853b6db877b370f5e044c1da
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-16 20:00:51 +0700

    Modified Controller

--- ServerBE/Controllers/AuthenticateController.cs
+++ ServerBE/Controllers/AuthenticateController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using ServerBE.Models;
 using ServerBE.Models.Auth;
-using ServerBE.Repository;
 using Shared.UserRole;
 using System;
 using System.Collections.Generic;
@@ -19,40 +20,98 @@ namespace ServerBE.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticateController : ControllerBase
+    public class AuthenticateController : Controller
     {
-        private readonly IAccountRepository _accountRepository;
+        private IIdentityServerInteractionService _interaction;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEventService _events;
+        private readonly IConfiguration _configuration;
 
-        public AuthenticateController(IAccountRepository accountRepository)
+        public AuthenticateController(
+            IIdentityServerInteractionService interaction,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            IEventService events,
+            IConfiguration configuration
+            )
         {
-            _accountRepository = accountRepository;
+            _interaction = interaction;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _events = events;
+            _configuration = configuration;
         }
 
         [HttpPost("signup")]
-        public async Task<IActionResult> SignUp([FromBody] SignUpModel signUpModel)
+        public async Task<IActionResult> SignUp([FromBody] SignUpModel signUpModel/*, string button*/)
         {
-            var result = await _accountRepository.SignUpAsync(signUpModel);
+            
+            //if (button.Equals("cancel"))
+            //{
+            //    return Redirect("~/");
+            //}
 
-            if (result.Succeeded)
+            //if (!ModelState.IsValid)
+            //{
+            //    return View("Sign Up", signUpModel);
+            //}
+
+            var user = new User()
             {
-                return Ok(result.Succeeded);
+                UserName = signUpModel.Username,
+                Email = signUpModel.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpModel.Password);
+
+            if (!result.Succeeded)
+            {
+                //return View("Register", RegisterVm);
+
+                return Content("Signed Up Unsuccesfully!!!");
+
             }
 
             //return Unauthorized();
-            return Content("Signed Up Succesfully!!!");
+            return Ok(result.Succeeded);
+
+            //await _userManager.AddToRoleAsync(user, "User");
+
+            //await _signInManager.SignInAsync(user, false);
+
+            //return Redirect(RegisterVm.ReturnUrl);
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] SignInModel signInModel)
+        public async Task<IActionResult> Login([FromBody] SignInModel signInModel/*, string button*/)
         {
-            var result = await _accountRepository.LoginAsync(signInModel);
+            var context = await _interaction.GetAuthorizationContextAsync(signInModel.ReturnUrl);
 
-            if (string.IsNullOrEmpty(result))
+            //if (button.Equals("cancel"))
+            //{
+            //    await _interaction.DenyAuthorizationAsync(context, AuthorizationError.AccessDenied);
+
+            //    return Redirect(context.RedirectUri);
+            //}
+
+            var result = await _signInManager.PasswordSignInAsync(signInModel.Username, signInModel.Password, false, false);
+
+            if (!result.Succeeded)
             {
-                return Unauthorized();
+                return Content("Invalid Username or Password!!!");
+                //ViewBag.Error = "Invalid Username or Password";
+                //return View("Login", signInModel);
             }
 
             return Ok(result);
+
+            //ViewBag.Error = null;
+
+            //return Redirect(loginVm.ReturnUrl);
         }
 
         //[HttpPost]
--- ServerBE/Controllers/ProductsController.cs
+++ ServerBE/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ using ServerBE.Models;
 namespace ServerBE.Controllers
 {
     [Authorize]
+    [EnableCors("AllowOrigins")]
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
