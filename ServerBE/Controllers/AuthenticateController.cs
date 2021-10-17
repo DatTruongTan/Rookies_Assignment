669ea20752262ae54222b383f869fcb204c6837a
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-17 21:02:44 +0700

    Authentication and authorization are ready to live

--- ServerBE/Controllers/AuthenticateController.cs
+++ ServerBE/Controllers/AuthenticateController.cs
@@ -1,4 +1,9 @@
-﻿using IdentityServer4.Services;
+﻿using IdentityModel;
+using IdentityServer4.Events;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -7,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using ServerBE.Models;
 using ServerBE.Models.Auth;
+using ServerBE.Options;
 using Shared.UserRole;
 using System;
 using System.Collections.Generic;
@@ -18,8 +24,8 @@ using System.Threading.Tasks;
 
 namespace ServerBE.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    //[Route("api/[controller]")]
+    //[ApiController]
     public class AuthenticateController : Controller
     {
         private IIdentityServerInteractionService _interaction;
@@ -27,15 +33,13 @@ namespace ServerBE.Controllers
         private readonly SignInManager<User> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IEventService _events;
-        private readonly IConfiguration _configuration;
 
         public AuthenticateController(
             IIdentityServerInteractionService interaction,
             UserManager<User> userManager,
             SignInManager<User> signInManager,
             RoleManager<IdentityRole> roleManager,
-            IEventService events,
-            IConfiguration configuration
+            IEventService events
             )
         {
             _interaction = interaction;
@@ -43,11 +47,16 @@ namespace ServerBE.Controllers
             _signInManager = signInManager;
             _roleManager = roleManager;
             _events = events;
-            _configuration = configuration;
         }
 
-        [HttpPost("signup")]
-        public async Task<IActionResult> SignUp([FromBody] SignUpModel signUpModel/*, string button*/)
+        [HttpGet]
+        public IActionResult SignUp(string returnUrl)
+        {
+            return View(new SignUpModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(/*[FromBody]*/ SignUpModel signUpModel/*, string button*/)
         {
             
             //if (button.Equals("cancel"))
@@ -70,24 +79,28 @@ namespace ServerBE.Controllers
 
             if (!result.Succeeded)
             {
-                //return View("Register", RegisterVm);
+                return View("SignUp", signUpModel);
 
-                return Content("Signed Up Unsuccesfully!!!");
+                //return Content("Signed Up Unsuccesfully!!!");
 
             }
+            //return Ok(result.Succeeded);
 
-            //return Unauthorized();
-            return Ok(result.Succeeded);
+            await _userManager.AddToRoleAsync(user, "User");
 
-            //await _userManager.AddToRoleAsync(user, "User");
+            await _signInManager.SignInAsync(user, false);
 
-            //await _signInManager.SignInAsync(user, false);
-
-            //return Redirect(RegisterVm.ReturnUrl);
+            return Redirect(signUpModel.ReturnUrl);
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] SignInModel signInModel/*, string button*/)
+        [HttpGet]
+        public IActionResult SignIn(string returnUrl)
+        {
+            return View(new SignInModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(/*[FromBody]*/ SignInModel signInModel, string button)
         {
             var context = await _interaction.GetAuthorizationContextAsync(signInModel.ReturnUrl);
 
@@ -102,16 +115,26 @@ namespace ServerBE.Controllers
 
             if (!result.Succeeded)
             {
-                return Content("Invalid Username or Password!!!");
-                //ViewBag.Error = "Invalid Username or Password";
-                //return View("Login", signInModel);
+                //return Content("Invalid Username or Password!!!");
+                ViewBag.Error = "Invalid Username or Password";
+                return View("SignIn", signInModel);
             }
 
-            return Ok(result);
+            //return Ok(result);
 
-            //ViewBag.Error = null;
+            ViewBag.Error = null;
 
-            //return Redirect(loginVm.ReturnUrl);
+            return Redirect(signInModel.ReturnUrl);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            await _signInManager.SignOutAsync();
+
+            var logoutRequest = await _interaction.GetLogoutContextAsync(logoutId);
+
+            return Redirect(logoutRequest.PostLogoutRedirectUri);
         }
 
         //[HttpPost]
