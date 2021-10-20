b8f840131b1fbb61fdff284f4c10220f95fc4fb0
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-20 21:14:57 +0700

    Prepare to build paging sorting

---
+++ ServerBE/Extensions/DataPagerExtension.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Constants;
+using Shared.Dto;
+using Shared.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerBE.Extensions
+{
+    public static class DataPagerExtension
+    {
+        public static async Task<PagedModelDto<Tmodel>> PaginateAsync<Tmodel>(
+            this IQueryable<Tmodel> query,
+            BaseQueryCriteriaDto criteriaDto,
+            CancellationToken cancellationToken
+            )
+            where Tmodel : class
+        {
+            var paged = new PagedModelDto<Tmodel>();
+
+            paged.CurrentPage = (criteriaDto.Page < 0) ? 1 : criteriaDto.Page;
+            paged.PageSize = criteriaDto.Limit;
+
+            if (!string.IsNullOrEmpty(criteriaDto.SortOrder.ToString()) &&
+                !string.IsNullOrEmpty(criteriaDto.SortColumn))
+            {
+                var sortOrder = criteriaDto.SortOrder == SortingEnum.Accsending ?
+                                                            ConstSortingPage.ASC :
+                                                            ConstSortingPage.DESC;
+                var orderString = $"{criteriaDto.SortColumn} {sortOrder}";
+                query = query.OrderBy(orderString);
+            }
+
+            var startRow = (paged.CurrentPage - 1) * paged.PageSize;
+
+            paged.Items = await query
+                                    .Skip(startRow)
+                                    .Take(paged.PageSize)
+                                    .ToListAsync(cancellationToken);
+
+            paged.TotalItems = await query.CountAsync(cancellationToken);
+            paged.TotalPages = (int)Math.Ceiling(paged.TotalItems / (double)paged.PageSize);
+
+            return paged;
+
+        }
+    }
+}
