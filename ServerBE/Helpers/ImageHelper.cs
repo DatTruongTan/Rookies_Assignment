88945d9328f6dc0400df729e7e7a6b106a8b5de0
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-20 21:13:15 +0700

    Add code help uploading the image to server

---
+++ ServerBE/Helpers/ImageHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ServerBE.Helpers
+{
+    public class ImageHelper
+    {
+        private const string IMAGE_FOLDER_NAME = "images";
+        public static string GetFileUrl(string fileName)
+        {
+            var url = string.Empty;
+            if (!string.IsNullOrEmpty(url))
+            {
+                url = $"{GetImageUrl()}//{fileName}";
+            }
+            return url;
+        }
+
+        public static string GetImageFolderPath()
+        {
+            return Path.Combine(WebHostEnvironmentHelper.GetWebRootPath(), IMAGE_FOLDER_NAME);
+        }
+
+        public static string GetImageUrl()
+        {
+            return $"{WebHostEnvironmentHelper.GetWebUrl()}//{IMAGE_FOLDER_NAME}";
+        }
+    }
+}
---
+++ ServerBE/Helpers/WebHostEnvironmentHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Helpers
+{
+    public class WebHostEnvironmentHelper
+    {
+        public static string GetWebRootPath()
+        {
+            var _ancessor = new HttpContextAccessor();
+
+            return _ancessor.HttpContext
+                            .RequestServices
+                            .GetRequiredService<IWebHostEnvironment>()
+                            .WebRootPath;
+        }
+
+        public static string GetWebUrl()
+        {
+            var _ancessor = new HttpContextAccessor();
+
+            return _ancessor.HttpContext
+                            .RequestServices
+                            .GetRequiredService<IConfiguration>()
+                            .GetValue<string>(ConstConfiguration.BACK_END_ENDPOINT);
+        }
+    }
+}
