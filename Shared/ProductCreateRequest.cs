31784c59bfae07bd9c8b60f86441a987d5b80f78
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-20 21:11:58 +0700

    Add request and view model for product controller

---
+++ Shared/ProductCreateRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shared
+{
+    public class ProductCreateRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public BrandEnum Brand { get; set; }
+        public GenderEnum Gender { get; set; }
+        public SizeEnum Size { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
---
+++ Shared/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Brand { get; set; }
+        public int Gender { get; set; }
+        public int Size { get; set; }
+        public int Rating { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
