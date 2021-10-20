988bc7e50867a6c8d35839ab0686394293f9a7a6
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-20 21:16:37 +0700

    Code admin role handler that update to startup

---
+++ ServerBE/Security/Authorization/Handlers/AdminRoleHandler.cs
@@ -0,0 +1,36 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Authorization;
+using ServerBE.Helpers;
+using ServerBE.Security.Authorization.Requirements;
+using Shared.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Security.Authorization.Handlers
+{
+    public class AdminRoleHandler : AuthorizationHandler<AdminRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                    AdminRoleRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == JwtClaimTypes.Role &&
+                                            c.Issuer == WebHostEnvironmentHelper.GetWebUrl()))
+            {
+                return Task.CompletedTask;
+            }
+
+            var adminClaim = context.User.FindFirst(c => c.Type == JwtClaimTypes.Role &&
+                                                      c.Issuer == WebHostEnvironmentHelper.GetWebUrl() &&
+                                                      c.Value == ConstSecurity.ADMINISTRATION_ROLE).Value;
+
+            if (!string.IsNullOrEmpty(adminClaim))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
