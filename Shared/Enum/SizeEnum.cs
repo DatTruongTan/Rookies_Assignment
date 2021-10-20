3b8ef04a89b9c83daa06d2043c47b111f0c65964
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-20 21:10:44 +0700

    Add more enum

---
+++ Shared/Enum/SizeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Enum
+{
+    public enum SizeEnum
+    {
+        Size38 = 38,
+        Size39 = 39,
+        Size40 = 40,
+        Size41 = 41,
+    }
+}
