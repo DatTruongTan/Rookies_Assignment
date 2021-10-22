0ccbf07ee2c56b6f89707316f8b17dfc0c2b2c05
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-22 21:00:14 +0700

    Create Dto

---
+++ CustomerFE/Mapping/BrandAutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CustomerFE.ViewModel;
+using CustomerFE.ViewModel.Product;
+using Shared.Dto;
+using Shared.Dto.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFE.Mapping
+{
+    public class BrandAutoMapperProfile : Profile
+    {
+        public BrandAutoMapperProfile()
+        {
+            CreateMap<ProductDto, ProductViewModel>().ReverseMap();
+            CreateMap<BaseQueryCriteriaDto, BaseQueryCriteriaVM>().ReverseMap();
+            CreateMap<PagedResponseDto<ProductDto>, PagedResponseVM<ProductViewModel>>().ReverseMap();
+        }
+    }
+}
