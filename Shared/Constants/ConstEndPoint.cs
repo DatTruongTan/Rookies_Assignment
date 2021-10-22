1bb483498ce4b0a9dd5b578ad7275373265a274b
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-22 16:00:36 +0700

    Add end point constant

---
+++ Shared/Constants/ConstEndPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Constants
+{
+    public class ConstEndPoint
+    {
+        public static string GET_PRODUCTS = "api/products";
+
+    }
+}
