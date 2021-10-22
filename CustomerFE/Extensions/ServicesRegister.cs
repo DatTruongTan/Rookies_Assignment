cd32b99cd28a94a7b515076f175171db1236f604
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-22 20:59:47 +0700

    Sign up services

---
+++ CustomerFE/Extensions/ServicesRegister.cs
@@ -0,0 +1,17 @@
+﻿using CustomerFE.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFE.Extensions
+{
+    public static class ServicesRegister
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IProductService, ProductService>();
+        }
+    }
+}
