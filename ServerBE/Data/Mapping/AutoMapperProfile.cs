222481096f8d275185eff0520965488129a73292
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-24 16:02:02 +0700

    Get location of image

---
+++ ServerBE/Data/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ServerBE.Helpers;
+using ServerBE.Models;
+using Shared.Dto.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Data.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(src => src.ImagePath,
+                           options => options.MapFrom(src => ImageHelper.GetFileUrl(src.ImageName)));
+        }
+    }
+}
