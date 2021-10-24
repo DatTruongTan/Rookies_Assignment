b34a7613550d359f16cc31477bd1759dd7cd6e7e
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-24 15:57:14 +0700

    Customer view model

---
+++ CustomerFE/ViewModel/BaseQueryCriteriaVM.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFE.ViewModel
+{
+    public class BaseQueryCriteriaVM
+    {
+        public string Search { get; set; }
+        public SortingEnum SortOrder { get; set; }
+        public string SortColumn { get; set; }
+        public int Limit { get; set; } = 9;
+        public int Page { get; set; } = 1;
+    }
+}
---
+++ CustomerFE/ViewModel/Product/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFE.ViewModel.Product
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
