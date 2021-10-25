3ffc0cbbe21618cfff9e9de27e9a650405f109dc
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-25 12:02:26 +0700

    Update query string variable

--- CustomerFE/Services/ProductService.cs
+++ CustomerFE/Services/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Shared;
 using Shared.Constants;
 using Shared.Dto;
@@ -7,31 +9,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace CustomerFE.Services
 {
     public class ProductService : IProductService
     {
         private readonly IHttpClientFactory _clientFactory;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConfiguration _configuration;
 
-        public ProductService(IHttpClientFactory clientFactory)
+        public ProductService(
+            IHttpClientFactory clientFactory, 
+            IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration)
         {
             _clientFactory = clientFactory;
+            _httpContextAccessor = httpContextAccessor;
+            _configuration = configuration;
         }
 
         public async Task<PagedResponseDto<ProductDto>> GetProductAsync(ProductCriteriaDto productCriteriaDto)
         {
             var client = _clientFactory.CreateClient(ConstService.BACK_END_NAMED_CLIENT);
-            //var client = _clientFactory.CreateClient("ServerBE");
 
-            var response = await client.GetAsync(ConstEndPoint.GET_PRODUCTS);
+            var Search = productCriteriaDto.Search;
+            var SortOrder = productCriteriaDto.SortOrder;
+            var SortColumn = productCriteriaDto.SortColumn;
+            var Page = productCriteriaDto.Page;
+            var Limit = productCriteriaDto.Limit;
 
-            //var queryString = $"searchString={productCriteriaDto.Search}";
-
-            //var response = await client.GetAsync($"{ConstEndPoint.GET_PRODUCTS}?{queryString}");
-            //var response = await client.GetAsync("api/products");
+            var queryString = $"Search={Search}&SortOrder={SortOrder}&SortColumn={SortColumn}&Limit={Limit}&Page={Page}";
+            var response = await client
+                .GetAsync($"https://localhost:44373/api/Products?{queryString}");
+            //.GetAsync($"{ConstConfiguration.BACK_END_ENDPOINT}/{ConstEndPoint.GET_PRODUCTS}?{queryString}");
 
             response.EnsureSuccessStatusCode();
             var pagedProducts = await response.Content.ReadAsAsync<PagedResponseDto<ProductDto>>();
