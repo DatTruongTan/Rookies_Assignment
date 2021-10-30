653f399ca996e7ae223861216e92750a5b449133
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-30 13:11:38 +0700

    Add mapper for rating model

--- CustomerFE/Mapping/BrandAutoMapperProfile.cs
+++ CustomerFE/Mapping/BrandAutoMapperProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using CustomerFE.ViewModel;
 using CustomerFE.ViewModel.Product;
+using CustomerFE.ViewModel.Rating;
 using Shared.Dto;
 using Shared.Dto.Product;
+using Shared.Dto.Rating;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,7 @@ namespace CustomerFE.Mapping
         public BrandAutoMapperProfile()
         {
             CreateMap<ProductDto, ProductViewModel>().ReverseMap();
+            CreateMap<RatingDto, RatingViewModel>().ReverseMap();
             CreateMap<BaseQueryCriteriaDto, BaseQueryCriteriaVM>().ReverseMap();
             CreateMap<PagedResponseDto<ProductDto>, PagedResponseVM<ProductViewModel>>().ReverseMap();
         }
