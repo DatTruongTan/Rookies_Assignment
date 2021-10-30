a7aa0e2da64301cca787ef9952496e2ec42cfdcc
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-30 13:18:56 +0700

    Add rating request

---
+++ Shared/RatingCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class RatingCreateRequest
+    {
+        public string ProductId { get; set; }
+        public string TextComment { get; set; }
+        public int RatingIndex { get; set; }
+        public string UserId { get; set; }
+    }
+}
