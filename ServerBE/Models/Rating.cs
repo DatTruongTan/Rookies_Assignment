497dc142161cf926913cd7860b3cd9f9b1cbb7f0
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-30 13:20:06 +0700

    Model for rating part

---
+++ ServerBE/Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBE.Models
+{
+    public class Rating
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string TextComment { get; set; }
+        public int RatingIndex { get; set; }
+        public string UserId { get; set; }
+    }
+}
