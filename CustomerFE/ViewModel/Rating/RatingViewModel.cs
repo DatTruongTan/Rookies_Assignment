16fe4c656dc6c23ced5bf2cf8454ee3f3f3ef9bb
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-30 13:09:42 +0700

    Add rating model

---
+++ CustomerFE/ViewModel/Rating/RatingViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFE.ViewModel.Rating
+{
+    public class RatingViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string ProductId { get; set; }
+        
+        [Required]
+        public string TextComment { get; set; }
+
+        [Required]
+        public int RatingIndex { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
---
+++ Shared/Dto/Rating/RatingDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Dto.Rating
+{
+    public class RatingDto
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string TextComment { get; set; }
+        public int RatingIndex { get; set; }
+        public string UserId { get; set; }
+    }
+}
