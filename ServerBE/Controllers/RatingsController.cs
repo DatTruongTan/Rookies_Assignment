592f425428b167645c7b5be3c3363c5f2a927587
dattruong1203 <73688051+dattruong1203@users.noreply.github.com>
2021-10-30 13:23:08 +0700

    Create controller for rating

---
+++ ServerBE/Controllers/RatingsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerBE.Data;
+using ServerBE.Models;
+using ServerBE.Services;
+using Shared;
+using Shared.ViewModels;
+
+namespace ServerBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IFileStorageService _fileStorageService;
+        private readonly IMapper _mapper;
+
+        public RatingsController(
+            ApplicationDbContext context,
+            IFileStorageService fileStorageService,
+            IMapper mapper
+            )
+        {
+            _context = context;
+            _fileStorageService = fileStorageService;
+            _mapper = mapper;
+        }
+
+        // GET: api/Ratings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rating>>> Getratings()
+        {
+            return await _context.ratings.ToListAsync();
+        }
+
+        // GET: api/Ratings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rating>> GetRating(string id)
+        {
+            var rating = await _context.ratings.FindAsync(id);
+
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return rating;
+        }
+
+        // PUT: api/Ratings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRating(string id, Rating rating)
+        {
+            if (id != rating.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rating).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RatingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ratings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductViewModel>> PostRating([FromBody] RatingCreateRequest request)
+        {
+            var rating = new Rating
+            {
+                Id = Guid.NewGuid().ToString(),
+                ProductId = request.ProductId,
+                TextComment = request.TextComment,
+                RatingIndex = request.RatingIndex,
+            };
+            _context.ratings.Add(rating);
+            await _context.SaveChangesAsync();
+
+
+            return CreatedAtAction(nameof(GetRating), new { id = rating.Id }, rating);
+        }
+
+        // DELETE: api/Ratings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRating(string id)
+        {
+            var rating = await _context.ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            _context.ratings.Remove(rating);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RatingExists(string id)
+        {
+            return _context.ratings.Any(e => e.Id == id);
+        }
+    }
+}
