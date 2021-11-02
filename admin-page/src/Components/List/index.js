66fd82101fadf5c05f255b16ee40c18e31adebc5
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-02 16:39:31 +0700

    Update layout for list product

--- admin-page/src/Components/List/index.js
+++ admin-page/src/Components/List/index.js
@@ -4,7 +4,8 @@ import { Table, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Endpoints from '../../Constants/endpoints';
 import { getBrandsRequest } from '../services/request';
-import { GET_ALL_PRODUCTS } from '../../Services/apiService';
+import UpdateProduct from '../Update';
+import { GET_ALL_PRODUCTS, DELETE_PRODUCT_ID } from '../../Services/apiService';
 import {
     ACCSENDING,
     DECSENDING,
@@ -24,6 +25,7 @@ const ListProduct = () => {
     useEffect(() => {
         GET_ALL_PRODUCTS().then((response) => {
             setProduct(response.data.items);
+            console.log(response.data.items);
         });
         // axios
         //     .get(process.env.REACT_APP_BACKEND_URL + Endpoints.products)
@@ -31,6 +33,24 @@ const ListProduct = () => {
         //         setProduct(response.data.items);
         //     });
     }, []);
+
+    const handleDeleteProduct = (event, id) => {
+        product.map((p) => p.id);
+        event.preventDefault();
+        axios
+            .delete(`${process.env.REACT_APP_BACKEND_API}/${id}`)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log('message: ' + err.message);
+            });
+        // DELETE_PRODUCT_ID(product.id).then((response) => {
+        //     console.log('Deleted Product -', response).catch((err) => {
+        //         console.log(err);
+        //     });
+        // });
+    };
 
     // useEffect(() => {
     //     async function fetchDataAsync() {
@@ -47,12 +67,13 @@ const ListProduct = () => {
             <Link to="/create" type="button" className="btn btn-success">
                 Create new Brand
             </Link>
-            {/* <Button variant="success">Create</Button> */}
             <Table striped bordered hover responsive>
                 <thead>
                     <tr>
                         <th>Name</th>
                         <th>Price</th>
+                        <th>Created Date</th>
+                        <th>Image</th>
                         <th>Function</th>
                     </tr>
                 </thead>
@@ -61,9 +82,46 @@ const ListProduct = () => {
                         <tr>
                             <td>{p.name}</td>
                             <td>{p.price}</td>
+                            <td>{p.createdDate}</td>
                             <td>
-                                <Button variant="primary">Edit</Button>
-                                <Button variant="danger">Delete</Button>
+                                <img
+                                    src={`${process.env.REACT_APP_BACKEND_URL}${p.imagePath}`}
+                                    alt="product"
+                                    height="50"
+                                    width="50"
+                                />
+                            </td>
+                            <td>
+                                {/* <Button variant="primary" type="submit">
+                                    Edit
+                                </Button> */}
+                                <Link
+                                    // to="/edit/:${id}"
+                                    to={`/edit/${p.id}`}
+                                    // UpdateProduct={p.id}
+                                    type="button"
+                                    className="btn btn-primary"
+                                >
+                                    Edit
+                                </Link>
+                                {/* <Link
+                                    // to="/edit/:${id}"
+                                    to="/delete"
+                                    // UpdateProduct={p.id}
+                                    type="button"
+                                    className="btn btn-danger"
+                                >
+                                    Delete
+                                </Link> */}
+                                <Button
+                                    variant="danger"
+                                    type="submit"
+                                    onClick={(event) =>
+                                        handleDeleteProduct(event)
+                                    }
+                                >
+                                    Delete
+                                </Button>
                             </td>
                         </tr>
                     ))}
