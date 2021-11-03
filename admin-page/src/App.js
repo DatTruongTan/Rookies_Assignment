e2d5fbf3b236c9bc8d4523f4df2508c63c272e9d
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-03 13:20:38 +0700

    Add authentication

--- admin-page/src/App.js
+++ admin-page/src/App.js
@@ -1,29 +1,86 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import React from 'react';
-import { CREATE_BRAND, HOME } from './Constants/pages';
+import React, { useState, useEffect } from 'react';
+import AuthService from './Services/auth-service';
+
+import { USER_PROFILE_STORAGE_KEY } from './Constants/oidc-config';
+
+import {
+    CREATE_PRODUCT,
+    EDIT_PRODUCT,
+    EDIT_PRODUCT_ID,
+    AUTH,
+    HOME,
+} from './Constants/pages';
 
 import CreateProduct from './Components/Create';
 import ListProduct from './Components/List';
+import UpdateProduct from './Components/Update';
+import Auth from './Components/Auth/Auth';
 // const CreateProduct = lazy(() => import('.Components/Create'));
 // const ListProduct = lazy(() => import('.Components/List'));
 // const UpdateProduct = lazy(() => import('./Update'));
 
 function App() {
+    const [userName, setUserName] = useState(undefined);
+
+    const handleLogin = (e) => {
+        AuthService.loginAsync();
+    };
+
+    const handleLogout = (e) => {
+        AuthService.logoutAsync();
+    };
+
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem(USER_PROFILE_STORAGE_KEY));
+        console.log('Thong tin user:', user);
+        if (user !== undefined) {
+            setUserName(user?.name);
+        }
+    }, []);
+
     return (
         <div className="">
             <h1>WELCOME!!!</h1>
+            <div>
+                <p> {userName} </p>
+                {userName === undefined ? (
+                    <button
+                        className="btn btn-danger"
+                        type="button"
+                        onClick={(e) => handleLogin()}
+                    >
+                        Login
+                    </button>
+                ) : (
+                    <button
+                        className="btn btn-danger"
+                        type="button"
+                        onClick={(e) => handleLogout()}
+                    >
+                        Logout
+                    </button>
+                )}
+            </div>
+
             <BrowserRouter>
                 <Switch>
                     <Route exact path={HOME}>
                         <ListProduct />
                     </Route>
-                    <Route exact path={CREATE_BRAND}>
+                    <Route path={AUTH}>
+                        <Auth />
+                    </Route>
+                    <Route exact path={CREATE_PRODUCT}>
                         <CreateProduct />
                     </Route>
-                    {/* <Route exact path={EDIT_BRAND}>
-                    <UpdateBrand />
-                </Route>  */}
+                    <Route exact path={EDIT_PRODUCT}>
+                        <UpdateProduct />
+                    </Route>
+                    {/* <Route exact path={DELETE_PRODUCT_ID}>
+                        <DeleteProduct id={ListProduct.id} />
+                    </Route> */}
                 </Switch>
             </BrowserRouter>
         </div>
