fa8eaea5c0cd288cd42d03834fa2edd9f650bc53
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-04 16:38:10 +0700

    Create Navbar and move login button to Navbar

--- admin-page/src/App.js
+++ admin-page/src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import AuthService from './Services/auth-service';
-
-import { USER_PROFILE_STORAGE_KEY } from './Constants/oidc-config';
 
 import {
     CREATE_PRODUCT,
@@ -17,70 +13,27 @@ import CreateProduct from './Components/Create';
 import ListProduct from './Components/List';
 import UpdateProduct from './Components/Update';
 import Auth from './Components/Auth/Auth';
-// const CreateProduct = lazy(() => import('.Components/Create'));
-// const ListProduct = lazy(() => import('.Components/List'));
-// const UpdateProduct = lazy(() => import('./Update'));
+import NavBar from './Components/Navbar';
 
 function App() {
-    const [userName, setUserName] = useState(undefined);
-
-    const handleLogin = (e) => {
-        AuthService.loginAsync();
-    };
-
-    const handleLogout = (e) => {
-        AuthService.logoutAsync();
-    };
-
-    useEffect(() => {
-        let user = JSON.parse(localStorage.getItem(USER_PROFILE_STORAGE_KEY));
-        console.log('Thong tin user:', user);
-        if (user !== undefined) {
-            setUserName(user?.name);
-        }
-    }, []);
-
     return (
-        <div className="">
-            <h1>WELCOME!!!</h1>
-            <div>
-                <p> {userName} </p>
-                {userName === undefined ? (
-                    <button
-                        className="btn btn-danger"
-                        type="button"
-                        onClick={(e) => handleLogin()}
-                    >
-                        Login
-                    </button>
-                ) : (
-                    <button
-                        className="btn btn-danger"
-                        type="button"
-                        onClick={(e) => handleLogout()}
-                    >
-                        Logout
-                    </button>
-                )}
-            </div>
-
+        <div>
+            <NavBar />
+            <h1 className="App">WELCOME!!!</h1>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path={HOME}>
-                        <ListProduct />
-                    </Route>
-                    <Route path={AUTH}>
-                        <Auth />
-                    </Route>
-                    <Route exact path={CREATE_PRODUCT}>
-                        <CreateProduct />
-                    </Route>
-                    <Route exact path={EDIT_PRODUCT}>
-                        <UpdateProduct />
-                    </Route>
-                    {/* <Route exact path={DELETE_PRODUCT_ID}>
-                        <DeleteProduct id={ListProduct.id} />
-                    </Route> */}
+                    <Route exact path={HOME} component={ListProduct} />
+                    <Route path={AUTH} component={Auth} />
+                    <Route
+                        exact
+                        path={CREATE_PRODUCT}
+                        component={CreateProduct}
+                    />
+                    <Route
+                        exact
+                        path={EDIT_PRODUCT}
+                        component={UpdateProduct}
+                    />
                 </Switch>
             </BrowserRouter>
         </div>
---
+++ admin-page/src/Components/Navbar/index.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+// import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import AuthService from '../../Services/auth-service';
+
+import { USER_PROFILE_STORAGE_KEY } from '../../Constants/oidc-config';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    const [userName, setUserName] = useState(undefined);
+
+    const handleLogin = (e) => {
+        AuthService.loginAsync();
+    };
+
+    const handleLogout = (e) => {
+        AuthService.logoutAsync();
+    };
+
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem(USER_PROFILE_STORAGE_KEY));
+        console.log('Thong tin user:', user);
+        if (user !== undefined) {
+            setUserName(user?.name);
+        }
+    }, []);
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="/">
+                        Admin Page
+                    </a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        class="d-flex justify-content-end align-items-center collapse navbar-collapse"
+                        id="navbarNav"
+                    >
+                        <ul class="d-flex align-items-center navbar-nav">
+                            <li class="nav-item me-3">{userName}</li>
+                            <li class="nav-item">
+                                {userName === undefined ? (
+                                    <button
+                                        className="btn btn-danger"
+                                        type="button"
+                                        onClick={(e) => handleLogin()}
+                                    >
+                                        Login
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="btn btn-danger"
+                                        type="button"
+                                        onClick={(e) => handleLogout()}
+                                    >
+                                        Logout
+                                    </button>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
