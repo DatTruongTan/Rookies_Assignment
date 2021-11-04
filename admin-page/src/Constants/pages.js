4d29ace19bf83b6ce887af7f9ade75db1539b6cd
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-04 15:50:04 +0700

    Update route

--- admin-page/src/Constants/pages.js
+++ admin-page/src/Constants/pages.js
@@ -2,9 +2,10 @@ export const LOGIN = '/login';
 export const AUTH = '/authentication/:action';
 export const HOME = '/';
 
-export const CREATE_BRAND = '/create';
-export const EDIT_BRAND = '/edit/:id';
-export const EDIT_BRAND_ID = (id) => `/edit/${id}`;
+export const CREATE_PRODUCT = '/create';
+export const EDIT_PRODUCT = '/edit/:id';
+export const EDIT_PRODUCT_ID = (id) => `/edit/${id}`;
+export const DELETE_PRODUCT_ID = (id) => `/delete/${id}`;
 
 export const UNAUTHORIZE = '/unauthorize';
 export const NOTFOUND = '/notfound';
