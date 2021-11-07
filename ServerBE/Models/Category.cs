dddb79e6440fe6ab8e8a480763a6a6343a3a42b0
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-07 20:10:58 +0700

    * UserController.cs: Api for viewing customers
    * Category.cs: Category Model
    
    * Role.cs: role model

---
+++ ServerBE/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServerBE.Models
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
