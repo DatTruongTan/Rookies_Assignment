a0f4089d21baea23bbaa3cb12bb24159ff230556
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-07 21:27:02 +0700

    Add constant to remove hard code

---
+++ Shared/Constants/ConstantUri.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Shared.Constants
+{
+    public static class ConstantUri
+    {
+        public static string BACK_END_URI = "https://localhost:5001/";
+        public static string FRONT_END_URI = "https://localhost:4001/";
+    }
+}
