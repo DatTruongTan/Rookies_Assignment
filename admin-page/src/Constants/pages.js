5a25cc35df8f13ba127dc551152e1ccd12943881
dattruong1203 <truongtandat9a3@outlook.com>
2021-11-10 11:39:16 +0700

    update url of category

--- admin-page/src/Constants/pages.js
+++ admin-page/src/Constants/pages.js
@@ -2,11 +2,17 @@ export const LOGIN = '/login';
 export const AUTH = '/authentication/:action';
 export const HOME = '/';
 export const USER = '/user';
+export const CATEGORY = '/category';
 
 export const CREATE_PRODUCT = '/create';
 export const EDIT_PRODUCT = '/edit/:id';
 export const EDIT_PRODUCT_ID = (id) => `/edit/${id}`;
 export const DELETE_PRODUCT_ID = (id) => `/delete/${id}`;
+
+export const CREATE_CATEGORY = '/category/create';
+export const EDIT_CATEGORY = '/category/edit/:id';
+export const EDIT_CATEGORY_ID = (id) => `/category/edit/${id}`;
+export const DELETE_CATEGORY_ID = (id) => `/category/delete/${id}`;
 
 export const UNAUTHORIZE = '/unauthorize';
 export const NOTFOUND = '/notfound';
